9fcb98e7c2f36a459819533b3d60a176662da7b3
Yuval Dekel <dekel@yelp.com>
2018-07-25 23:35:25 -0700

    settings file to use environment variables

--- fast_bird_part_localization/settings.py
+++ fast_bird_part_localization/settings.py
@@ -1,10 +1,11 @@
+import os
 # Caffe Settings
-CAFFE_PYTHON_PATH = '/home/yasser/installs/caffe/python'
-CAFFE_NET_PRETRAINED = '/home/yasser/installs/caffe/models/bvlc_reference_caffenet/bvlc_reference_caffenet.caffemodel'
-GPU_MODE = True
+CAFFE_PYTHON_PATH = os.environ.get('CAFFE_PYTHON_PATH','/home/yasser/installs/caffe/python')
+CAFFE_NET_PRETRAINED = os.environ.get('CAFFE_NET_PRETRAINED','/home/yasser/installs/caffe/models/bvlc_reference_caffenet/bvlc_reference_caffenet.caffemodel')
+GPU_MODE = False
 
 # Dataset Settings
-CUB_FOLDER_PATH = '/home/ipl/datasets/CUB-200-2011/CUB_200_2011/CUB_200_2011'
+CUB_FOLDER_PATH = os.environ.get('CUB_FOLDER_PATH', '/home/ipl/datasets/CUB-200-2011/CUB_200_2011/CUB_200_2011')
 
 # Algorithm Settings
 POISSON_PART_RADIUS = {'head': 15, 'body': 30}
